950d09c9e87ee28fb4bf9d99cf67919c36d802ab
Satyam Chourasia <satyamchourasia@Satyams-MacBook-Pro.local>
2024-04-24 01:35:20 +0530

    blue

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,13 +10,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ColoredBox(
-<<<<<<< HEAD
-      color: Colors.red,
-=======
-
-      color: Colors.green,
-
->>>>>>> 225ea489527a80ac8e60859472ad32b328744e97
+      color: Colors.blue,
       child: SizedBox(
         child: ElevatedButton(
           onPressed: () {
